a6274e3598f4f54a2ea5322d42f5388b15324a33
suraj2502 <shuklasuraj620@gmail.com>
2020-06-26 00:32:15 +0530

    first commit

---
+++ Model/List.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test');
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected successfully")
+});
+var ListSchema = new mongoose.Schema({
+  title: String,
+  link: String,
+  author: String,
+  age:String,
+  points: Number,
+  comments: String,
+  rank: Number
+ 
+});
+module.exports = mongoose.model('List', ListSchema);
---
+++ index.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { html } = require('cheerio');
+const list=require('./Model/List');
+const url="https://news.ycombinator.com/news?p=";
+var db,data
+
+var lnk=[]
+   
+   axios.get(url).then(response=>{
+        let getdata=(html)=>{
+            data=[]
+            const $=cheerio.load(html);
+         
+            $('span.comhead').each((i,ele)=>{
+                let a = $(ele).prev()
+              let title = a.text()
+          let link = a.attr('href')
+          let rank = a.parent().parent().text()
+          let subtext = a.parent().parent().next().children('.subtext').children()
+          let author = $(subtext).eq(1).text()
+          let age = $(subtext).eq(2).text()
+          let points = $(subtext).eq(0).text()
+          let comments = $(subtext).eq(5).text()
+          data.push({
+            title: title,
+             link: link,
+             author: author,
+             age:age,
+             points: parseInt(points),
+             comments: comments,
+             rank: parseInt(rank)
+          })
+          lnk.push({link:link})
+            })
+      
+           return data;
+        }
+        list.create(getdata(response.data))
+        console.log(getdata(response.data))
+       //db=new list(getdata(response.data))
+       //db.save()
+    }).catch(error=>{
+        console.log(error);
+    })
